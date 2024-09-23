62360af9fd3a24cd3189ce33b7c4909269225f54
Aimad MAJDOU <amajdou@squad.fr>
2024-09-23 16:09:49 +0200

    simple setup

---
+++ src/app/providers.tsx
@@ -0,0 +1,8 @@
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  // Add providers here
+  return <>{children}</>;
+}
