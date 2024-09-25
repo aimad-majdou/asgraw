0f1b4dac9dffb0166044fd881f36a2b2c589945e
Aimad MAJDOU <aimad.majdou@gmail.com>
2024-09-25 15:45:51 +0200

    add logger for prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,7 @@
+import { logger } from "@/lib/logger";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import ws from "ws";
 
 // Set WebSocket constructor for Neon configuration
@@ -18,11 +19,59 @@ const adapter = new PrismaNeon(pool);
 // Create a new PrismaClient instance with the Neon adapter
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma =
+export const prisma: PrismaClient<
+  Prisma.PrismaClientOptions,
+  Prisma.LogLevel | Prisma.LogDefinition,
+  any
+> =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
-    // You can include other options here if necessary
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "event",
+        level: "error",
+      },
+      {
+        emit: "event",
+        level: "info",
+      },
+      {
+        emit: "event",
+        level: "warn",
+      },
+    ],
   });
+
+function sanitizeParams(params: string): string {
+  // Implement sanitization logic based on your application's needs
+  return params.replace(/password=.*?(&|$)/, "password=***$1");
+}
+
+// Listener for 'query' events
+prisma.$on("query", (e) => {
+  logger.debug("Prisma::Query:" + e.query.replace(/\\"/g, '"'));
+  logger.debug("Prisma::Params:" + sanitizeParams(e.params));
+  logger.debug("Prisma::Duration:" + e.duration + "ms");
+});
+
+// Listener for 'info' events
+prisma.$on("info", (e) => {
+  logger.info(`Prisma: ${e.message}`);
+});
+
+// Listener for 'warn' events
+prisma.$on("warn", (e) => {
+  logger.warn(`Prisma: ${e.message}`);
+});
+
+// Listener for 'error' events
+prisma.$on("error", (e) => {
+  logger.error(`Prisma: ${e.message}`);
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
